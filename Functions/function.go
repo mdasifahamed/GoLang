32093acf28b575c1cd0b8bf6da5d868b9d00a086
mdasifahamed <mdasifahamed89@yahoo.com>
2024-03-04 12:31:38 +0600

    parametrized functions

--- Functions/function.go
+++ Functions/function.go
@@ -8,6 +8,26 @@ func displaygo() {
 	fmt.Println("hello go")
 }
 
+// parametrized function
+func calculateInterestYearly(amount int, rate float32) float32 {
+	totalInterest := (float32(amount) * rate) / 100.0
+	return totalInterest
+}
+
+func calculateInterestMonthly(amount int, rate float32) float32 {
+	monthlyProfit := calculateInterestYearly(amount, rate) / 12.0
+	return monthlyProfit
+}
 func main() {
 	displaygo()
+
+	// 1524700 for 10% Yearly Interrest Would be
+	amountToinvest := 1524700
+	var rate float32 = 10.0
+
+	profit := calculateInterestYearly(amountToinvest, rate)
+	fmt.Println("Profit generated in a Year", profit)
+	monthlyReturn := calculateInterestMonthly(amountToinvest, rate)
+	fmt.Println("Profit generated in a month", monthlyReturn)
+
 }
