5d5c22491a463f6cb1ea0090e6b87caac5bf1cd2
mdasifahamed <mdasifahamed89@yahoo.com>
2024-03-06 14:32:44 +0600

    Use of generics

--- Generics/generics.go
+++ Generics/generics.go
@@ -1,6 +1,8 @@
 package main
 
-type Ordered interface {
+import "fmt"
+
+type AnyType interface {
 	int | float32 | ~string
 }
 
@@ -17,13 +19,28 @@ func add(a int, b int) int {
 	return a + b
 }
 
-// in those generics comes in handy we can define a fucntion with type of any and use it
+// in those cases generics comes in handy we can define a fucntion with type of as per our requirments
+// in an interface and use those types in tha sam functon to perofrm operations
 // as per our required data type
-
-func genericsAdd[T Ordered](a, b T) T {
+// [T AnyType] `AnyType` interface has three types in it we use those in th following
+// / `genericsAdd` function
+func genericsAdd[T AnyType](a, b T) T {
 	return a + b
 }
 
 func main() {
+
+	r := add(12, 52)
+	// r:=add(12.5,52.5) // we cannot pass float to it becasuseit takes only int type as parameter
+
+	// but we can do it in `genreicsAdd` function as it uses generics to deetrmine types
+	r2 := genericsAdd[int](10, 12)
+	r3 := genericsAdd[float32](10.68, 12.694)
+	r4 := genericsAdd[string]("GO", "Lang")
+
+	fmt.Println("Addintion of two integer from `add()` ", r)
+	fmt.Println("Addintion of two integer from `generics()` ", r2)
+	fmt.Println("Addintion of two floats from `generics()` ", r3)
+	fmt.Println("Concatation of two stirng from `generics()` ", r4)
 
 }
